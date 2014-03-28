f5b97c8914ad349e7a0272ca2035f77541e0d3e2
Benoit Perroud <benoit@noisette.ch>
2014-03-28 14:11:38 +0100

    Remove un-used import

--- src/main/java/org/apache/hadoop/hdfs/server/datanode/VolumeBalancer.java
+++ src/main/java/org/apache/hadoop/hdfs/server/datanode/VolumeBalancer.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
