939f520bc92aabd48b81077a19211001dfbded5b
Benoit Perroud <benoit@noisette.ch>
2014-04-01 10:24:59 +0200

    Do variable replacement using maven-resources plugin. Note filtering replace only one occurence of the variable per line, so store in a local variable Move from /var/lib to /opt

--- src/main/scripts/cdh-balance-local-volumes.sh
+++ src/main/scripts/cdh-balance-local-volumes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DATE=$(date +%s)
+ROOT_FOLDER=${rpm.path}
 
 ## Copy the latest hdfs-site.xml from the Datanode process to a tmp location and grant read permission for hdfs user.
 mkdir /tmp/volume-balancer-${DATE}
@@ -8,5 +9,5 @@ cp /var/run/cloudera-scm-agent/process/$(ls -1rt /var/run/cloudera-scm-agent/pro
 chmod 755 /tmp/volume-balancer-${DATE}/*
 
 ## Run the balancer as hdfs, with classpath hack to include hdfs-site.xml but exclude the log4j.properties provided by cloudera-scm-agent.
-sudo -u hdfs HADOOP_USER_CLASSPATH_FIRST=true HADOOP_CLASSPATH=/tmp/volume-balancer-${DATE} hadoop jar @@ROOT_FOLDER@@/lib/volume-balancer-@@VERSION@@.jar org.apache.hadoop.hdfs.server.datanode.VolumeBalancer $@ | tee -a @@ROOT_FOLDER@@/logs/log-$(date +%s).log
+sudo -u hdfs HADOOP_USER_CLASSPATH_FIRST=true HADOOP_CLASSPATH=/tmp/volume-balancer-${DATE} hadoop jar ${ROOT_FOLDER}/lib/volume-balancer-${project.version}.jar org.apache.hadoop.hdfs.server.datanode.VolumeBalancer $@ | tee -a ${ROOT_FOLDER}/logs/log-$(date +%s).log
 
